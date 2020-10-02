5edbf64c90f4b43e20e7089a9e676220e3a3bd7a
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-02 13:58:20 +0300

    Add project files.

---
+++ UserList/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserList
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<User> users;
+        private User selectedUser;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            users = new ObservableCollection<User>()
+            {
+                new User() { Id=0, FirstName="Anton", LastName="Tymoshchuk", Phone="0666865259" }
+            };
+            usersListBox.ItemsSource = users;
+        }
+
+        private void UsersListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedUser = usersListBox.SelectedItem as User;
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ UserList/User.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Media.Imaging;
+
+namespace UserList
+{
+    class User : INotifyPropertyChanged
+    {
+        private int id;
+        private string firstName;
+        private string lastName;
+        private string phone;
+        private BitmapSource avatar;
+
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                firstName = value;
+                OnPropertyChanged("FirstName");
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                lastName = value;
+                OnPropertyChanged("LastName");
+            }
+        }
+
+        public string Phone
+        {
+            get { return phone; }
+            set
+            {
+                phone = value;
+                OnPropertyChanged("Phone");
+            }
+        }
+
+        public BitmapSource Avatar
+        {
+            get { return avatar; }
+            set
+            {
+                avatar = value;
+                OnPropertyChanged("Avatar");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
